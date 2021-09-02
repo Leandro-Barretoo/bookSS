1e09956206cf09cab9e8951dd8342c44f1f365de
Leandro <officialdevmi@gmail.com>
2021-09-02 15:10:27 -0700

    Add redux-thunk and create middleware to fetch all books from api

--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,14 +1,26 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
-import booksReducer from './books/books';
+import thunk from 'redux-thunk';
+import booksReducer, { addBook } from './books/books';
 
 const rootReducer = combineReducers({
   booksReducer,
 });
 
+const fetchBooks = () => (dispatch) => {
+  fetch('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/ym9eiorepf3iLhrJ7Q7F/books')
+    .then((response) => {
+      dispatch(addBook(response.json()));
+    });
+};
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(logger),
+  applyMiddleware(thunk),
 );
 
+store.subscribe(() => { console.log(store.getState()); });
+store.dispatch(fetchBooks());
+
 export default store;
+
+// app id: ym9eiorepf3iLhrJ7Q7F
