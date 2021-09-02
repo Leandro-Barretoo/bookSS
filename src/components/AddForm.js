8ef82d41503b2732b3b91278dac5f63ac0f9b337
Leandro <officialdevmi@gmail.com>
2021-09-02 04:46:12 -0700

    Add books reducer with respective actions

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook, removeBook } from '../redux/books/books';
+
+const dispatch = useDispatch();
+
+const submitBookToStore = () => {
+  const newBook = {
+    id,
+    title,
+    author: 'unknown',
+  }
+}
 
 const AddForm = () => (
   <div>
     <span>ADD NEW BOOK</span>
     <form>
-      <input />
+      <input type="text" />
       <input />
       <button type="submit">ADD BOOK</button>
     </form>
--- src/redux/cofigureStore.js
+++ src/redux/cofigureStore.js
@@ -1,36 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
 
-const initialState = {
-  books: [
-    {
-      id: 0,
-      categorie: 'Action',
-      title: 'The Hunger Games',
-      author: 'Suzanne Collins',
-    },
-    {
-      id: 1,
-      categorie: 'Science Fiction',
-      title: 'Dune',
-      author: 'Frank Herbert',
-    },
-    {
-      id: 2,
-      categorie: 'Economy',
-      title: 'Capital in the Twenty-First Century',
-      author: 'Suzanne Collins',
-    },
-  ],
-};
+const rootReducer = combineReducers({
+  booksReducer,
+});
 
-function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
-
-const store = createStore(rootReducer);
-window.store = store;
+const store = createStore(
+  rootReducer,
+  applyMiddleware(logger),
+);
 
 export default store;
